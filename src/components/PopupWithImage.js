b26acb41f92e2f8b85a140f3e1e1e1fdda118511
Korvin <hofilenakorvin@gmail.com>
2024-08-28 04:15:44 -0700

    updated the PopupWithForm.js class to include all necessary API interaction and UX improvements

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -1,18 +1,47 @@
 import Popup from "./Popup.js";
 
-class PopupWithImage extends Popup {
-  constructor(popupSelector) {
+class PopupWithForm extends Popup {
+  constructor(popupSelector, { handleFormSubmit }) {
     super(popupSelector);
-    this._image = this._popupElement.querySelector(".modal__image");
-    this._caption = this._popupElement.querySelector(".modal__caption");
+    this._handleFormSubmit = handleFormSubmit;
+    this._form = this._popupElement.querySelector(".modal__form");
+    this._inputList = this._form.querySelectorAll(".modal__input");
+    this._submitButton = this._form.querySelector(".modal__button");
+    this._submitButtonText = this._submitButton.textContent;
   }
 
-  open({ name, link }) {
-    this._image.src = link;
-    this._image.alt = name;
-    this._caption.textContent = name;
-    super.open();
+  _getInputValues() {
+    const formValues = {};
+    this._inputList.forEach((input) => {
+      formValues[input.name] = input.value;
+    });
+    return formValues;
+  }
+
+  setEventListeners() {
+    super.setEventListeners();
+    this._form.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+      this._renderLoading(true);
+      this._handleFormSubmit(this._getInputValues())
+        .then(() => this.close())
+        .catch((err) => console.error(`Error: ${err}`))
+        .finally(() => this._renderLoading(false));
+    });
+  }
+
+  close() {
+    super.close();
+    this._form.reset();
+  }
+
+  _renderLoading(isLoading) {
+    if (isLoading) {
+      this._submitButton.textContent = "Saving...";
+    } else {
+      this._submitButton.textContent = this._submitButtonText;
+    }
   }
 }
 
-export default PopupWithImage;
+export default PopupWithForm;
